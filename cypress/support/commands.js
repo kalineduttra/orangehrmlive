09e7e56ab2d588866697a6417fcaff69f6d19c69
Kaline Dutra <kalineduttra@gmail.com>
2024-10-09 22:18:37 -0300

    add custom command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,16 @@
+// Login
+Cypress.Commands.add('login', (username,password) => {
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username);
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password);
+    cy.get('.oxd-button').click();
+})
+
+// Admin
+Cypress.Commands.add('admin', (username) => {
+    cy.get(':nth-child(1) > .oxd-main-menu-item > .oxd-text').click();
+    cy.get(':nth-child(2) > .oxd-input').type(username);
+    cy.get('.oxd-form-actions > .oxd-button--secondary').click;
+})
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
