50074142628c61369b8bf8170e28124835b30feb
Kaline Dutra <kalineduttra@gmail.com>
2024-10-13 23:48:43 -0300

    test: check visibility of registration not found message

--- cypress/e2e/userManagement.cy.js
+++ cypress/e2e/userManagement.cy.js
@@ -4,7 +4,9 @@ describe('Test Scenario', () => {
     })
     it(`search for a user that doesn't exist in the system`, () => {
       cy.login('Admin','admin123')
-      cy.admin('Ana')
+      cy.admin('Ana', ' ')
+      cy.contains('No Records Found').should('be.visible')
+      cy.get('.oxd-toast').should('be.visible').contains('No Records Found');
 
       cy.request('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers')
         .then((res) => {
