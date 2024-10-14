b7aaea00c0bc000a338226e19f55ad6b549eca61
Kaline Dutra <kalineduttra@gmail.com>
2024-10-13 23:43:05 -0300

    test(refactory): improve sensitive data code structure

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,24 +1,28 @@
 describe('Test Scenario', () => {
   beforeEach ('URL visit', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
   })
   it('Fill in the login fields with valid data and authenticate the user on the page', () => {
-    cy.login('Admin','admin123')
+    cy.login(Cypress.env('validUser'), Cypress.env('validPassword'));
   })
   it('Invalid Username', () => {
-    cy.login('Owner','admin123')
+    cy.login(Cypress.env('invalidUser'), Cypress.env('validPassword'))
     cy.contains('Invalid credentials').should('be.visible');
   })
   it('Invalid Password', () => {
-    cy.login('Admin','owner12')
+    cy.login(Cypress.env('validUser'), Cypress.env('invalidPassword'))
     cy.contains('Invalid credentials').should('be.visible');
   })
   it('Case Sensitive Username', () => {
-    cy.login('admin','owner12')
+    cy.login(Cypress.env('caseSensitiveUsername'), Cypress.env('validPassword'))
     cy.contains('Invalid credentials').should('be.visible');
   })
   it('Case Sensitive Password', () => {
-    cy.login('admin','Owner12')
+    cy.login(Cypress.env('validUser'), Cypress.env('caseSensitivePassword'))
     cy.contains('Invalid credentials').should('be.visible');
+  })
+  it('Unfilled credentials', () => {
+    cy.login(' ', ' ')
+    cy.contains('Required').should('be.visible');
   })
 })
