c2d5d5bfdb1ac1a3170fa0c87a176ef0ab99abb2
Kaline Dutra <kalineduttra@gmail.com>
2024-10-14 21:47:50 -0300

    test: updt script

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,28 +1,27 @@
 describe('Test Scenario', () => {
-  beforeEach ('URL visit', () => {
-    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-  })
+  
   it('Fill in the login fields with valid data and authenticate the user on the page', () => {
-    cy.login(Cypress.env('validUser'), Cypress.env('validPassword'));
+    cy.login(Cypress.env('validUser'), Cypress.env('validPassword'))
+    cy.url().should('contain', '/dashboard/index')
   })
   it('Invalid Username', () => {
     cy.login(Cypress.env('invalidUser'), Cypress.env('validPassword'))
-    cy.contains('Invalid credentials').should('be.visible');
+    cy.contains('Invalid credentials').should('be.visible')
   })
-  it('Invalid Password', () => {
+  it('Invalid Passw ord', () => {
     cy.login(Cypress.env('validUser'), Cypress.env('invalidPassword'))
-    cy.contains('Invalid credentials').should('be.visible');
+    cy.contains('Invalid credentials').should('be.visible')
   })
   it('Case Sensitive Username', () => {
     cy.login(Cypress.env('caseSensitiveUsername'), Cypress.env('validPassword'))
-    cy.contains('Invalid credentials').should('be.visible');
+    cy.contains('Invalid credentials').should('be.visible')
   })
   it('Case Sensitive Password', () => {
     cy.login(Cypress.env('validUser'), Cypress.env('caseSensitivePassword'))
-    cy.contains('Invalid credentials').should('be.visible');
+    cy.contains('Invalid credentials').should('be.visible')
   })
   it('Unfilled credentials', () => {
     cy.login(' ', ' ')
-    cy.contains('Required').should('be.visible');
+    cy.contains('Required').should('be.visible')
   })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,8 +1,9 @@
 // Login
 Cypress.Commands.add('login', (username,password) => {
-    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username);
-    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password);
-    cy.get('.oxd-button').click();
+  cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+  cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username);
+  cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password);
+  cy.get('.oxd-button').click();
 })
 
 // Reset Password
