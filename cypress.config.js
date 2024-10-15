e959c7f341a64babe52d3225ef94bd26619c8121
Kaline Dutra <kalineduttra@gmail.com>
2024-10-15 13:21:22 -0300

    config github actions setup

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "hz4t21",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
