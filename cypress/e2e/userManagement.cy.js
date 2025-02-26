b3595c2c9e70bb781c0313f024105fb4e0bdf5e2
Kaline Dutra <kalineduttra@gmail.com>
2025-02-25 22:52:05 -0300

    updt variables

--- cypress/e2e/userManagement.cy.js
+++ cypress/e2e/userManagement.cy.js
@@ -1,18 +1,44 @@
-describe('Test Scenario', () => {
-  beforeEach(() => {
-    cy.login1('/login')
-  })
-    it(`search for a user that doesn't exist in the system`, () => {
-      //cy.login('Admin','admin123')
-      cy.admin('Ana', ' ')
-      cy.contains('No Records Found').should('be.visible')
-      cy.get('.oxd-toast').should('be.visible').contains('No Records Found');
+const { validUser, validPassword } = Cypress.env()
+const username = 'Ana25'
+const requiredMessage = 'Required'
 
-      cy.request('GET', 'https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewSystemUsers')
+
+describe('Given that the user navigates to the Admin screen', () => {
+  beforeEach(() => {
+    cy.createSession(validUser, validPassword)
+    cy.visit('/dashboard/index')
+    cy.get('.oxd-sidepanel-body').contains('Admin').click()
+  })
+  context('When searching for a user that does not exist in the system', () => {
+    it(`Then it should display a message that it was not found`, () => {
+      cy.get('.oxd-table-filter').find(':nth-child(2) > .oxd-input').type(username)
+      cy.get('.oxd-form-actions > .oxd-button--secondary').click()
+      cy.get('[id="oxd-toaster_1"]').contains('No Records Found')
+
+      cy.request('GET', '/admin/viewSystemUsers')
         .then((res) => {
           expect(res.status).to.eq(200)
           expect(res.body).is.not.empty
         })
-      cy.get('.oxd-button--ghost').click()
     })
+  })
+
+  context.only('When click on add a user', () => {
+    beforeEach(() => {
+      cy.get('.orangehrm-header-container > .oxd-button').click()
+    })
+    it('Then when leaving the form blank it should display a message', () => {
+      cy.get('.oxd-button--secondary').click()
+      cy.contains(requiredMessage)
+      cy.contains('Passwords do not match')
+    })
+    it('Then when they click on cancel the user should be redirected to the User Management screen', () => {
+      cy.get('.oxd-button--ghost').click()
+      cy.url().should('contain', '/admin/viewSystemUsers')
+    })
+  })
 })
+
+
+      //cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click()
+      //cy.get('.oxd-select-dropdown > :nth-child(2)').click()
